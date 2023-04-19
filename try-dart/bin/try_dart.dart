39032c24eea716b82e28d40c13eded26ba16a903
Konstantin Shcherban <kostya.scherban@gmail.com>
2023-04-19 19:45:50 +0300

    Try Dart

---
+++ try-dart/bin/try_dart.dart
@@ -0,0 +1,5 @@
+import 'package:try_dart/try_dart.dart' as try_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${try_dart.calculate()}!');
+}
---
+++ try-dart/test/try_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:try_dart/try_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
